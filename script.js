5b8df2ca62b11aed785e4bc16682296d7d8d6bff
ad3rinto <aad3rinto@gmail.com>
2022-12-25 20:26:58 +0000

    refactor set gradient function

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ var colour1 = document.querySelector('.colour1')
 var colour2 = document.querySelector('.colour2')
 var body = document.querySelector('body')
 
+function setGradient() {
+    body.style.background = "linear-gradient(to right, " 
+    + colour1.value + ", " 
+    + colour2.value + ")";
 
+}
 
-colour1.addEventListener('input', function(){
-    body.style.background = "linear-gradient(to right, "+ colour1.value + ", " + colour2.value + ")";
+colour1.addEventListener('input',setGradient )
 
-})
-
-colour2.addEventListener('input', function () {
-    body.style.background = "linear-gradient(to left, " + colour1.value + ", " + colour2.value + ")";
-
-})
+colour2.addEventListener('input', setGradient)
